b358492c16aaffdd7c760a92b1c8bb59425a8b57
Nicolas-85 <63639978+Nicolas-85@users.noreply.github.com>
2022-07-20 17:26:18 -0300

    Commit de prueba
    Subo un text para probar una cosa.

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -2,6 +2,8 @@ import {Routes, Route, BrowserRouter} from 'react-router-dom'
 import './App.css';
 import Landing from "./pages/landing/Landing";
 
+//Esto es una prueba para gitHub------------------------------------
+
 
 function App() {
   return (
