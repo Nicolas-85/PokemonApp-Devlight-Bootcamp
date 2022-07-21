a6b3a7efbc27a75c8ac00911887c9b4b56b9c4f3
Nicolas-85 <63639978+Nicolas-85@users.noreply.github.com>
2022-07-20 22:35:42 -0300

    with Router
    commit para seguir en la otra pc

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -2,17 +2,19 @@ import {Routes, Route, BrowserRouter} from 'react-router-dom'
 import './App.css';
 import Landing from "./pages/landing/Landing";
 
-//Esto es una prueba para gitHub------------------------------------
-
 
 function App() {
+  
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element= {<Landing/>}/>
+        <Route path='/' element= {isLoggged? <List/> : <Landing/>}/>
+        <Route path=''/>
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+ 
--- pokemon-app/src/pages/Login/Login.jsx
+++ pokemon-app/src/pages/Login/Login.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react"
 import LoginForm from "./LoginForm"
+import { useNavigate } from 'react-router-dom';
 
 const Login = ()=> {
+
+   //Hooks
    const [isLogged, setIsLogged] = useState(window.localStorage.getItem('isLogged'))
+   const navigate = useNavigate()
+
 
    const onSucces = ()=>{//esta función la pasamos como prop para que se vuelva a renderizar el componente
       setIsLogged(true)  //luego de cada ejecución de LoginForm a través de la modificación del estado.
+      navigate('list')
    }
    return(
       <LoginForm onSucces={onSucces}/> 
